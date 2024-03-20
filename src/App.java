13eee58e20696245df2d97b979df758a28db0f02
Aoyon Mir <aoyon.mir@elev.ga.ntig.se>
2024-03-20 10:36:04 +0100

    Kopierat in koden

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+    int slump = (int) (Math.random()*100);
+    slump-=49;
+    if(slump<0)
+    System.out.println("slump blev negativ");
+    
     }
 }
