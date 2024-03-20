7a0e65953b8c91d1a4d43ac79d20035e8377186c
Aoyon Mir <aoyon.mir@elev.ga.ntig.se>
2024-03-20 11:36:50 +0100

    Kopierat koden från del d

--- src/App.java
+++ src/App.java
@@ -1,16 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        int slump = (int)Math.random()*100;
-        slump-=49;
-        if(slump<0)
-        {
-        System.out.println("Slumptalet är negativt");
-        }
-        else
-        {
-        System.out.println("Slumptalet");
-        }
-
+    String cx="a";
+    int nummret =0;
+    System.out.println("Då kör vi, vilket nummer avbryter programmet");
+    do{
+    System.out.println("En gång till");
+    nummret=(int) ((Math.random()*5+1)+9);
+    System.out.println("Numret xxxx");
+    }while(!cx.equals(Integer.toHexString(nummret)));
+    System.out.println("Rätt nummer framslumpat SLUT");
     }
+    
 }
