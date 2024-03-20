fdf63082c0d9d76037781ecc45d42b4f372ce45a
Aoyon Mir <aoyon.mir@elev.ga.ntig.se>
2024-03-20 10:56:29 +0100

    Kopierat kod från del a

--- src/App.java
+++ src/App.java
@@ -1,10 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
-    int slump = (int) (Math.random()*100);
-    slump-=49;
-    if(slump<0)
-    System.out.println("slump blev negativ");
+    int[] array = {3,2,5,4,6,8,9,7};
     
+    for (int i = 0; i < array.length; i++) {
+    System.out.println(i+" har värdet "+array[i]);
+    }
+
     }
 }
